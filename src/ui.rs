7cb7c747965ac05b39bde1857884665005899750
Steven Briggs <gshbriggs@gmail.com>
2016-02-21 22:26:50 -0500

    Convert all tabs to spaces

--- src/ui.rs
+++ src/ui.rs
@@ -15,48 +15,48 @@ pub struct Ui {
 
 impl Ui {
 
-	/// Initializes a new Ui struct
-	pub fn new() -> Self {
-		Ui {
-			board: Window::new(0, 0, 11, 22),
-			score: Window::new(12, 0, 11, 2),
-			level: Window::new(12, 4, 11, 2),
-			lines: Window::new(12, 8, 11, 2),
-			next_piece: Window::new(12, 12, 11, 6)
-		}
-	}
+    /// Initializes a new Ui struct
+    pub fn new() -> Self {
+        Ui {
+            board: Window::new(0, 0, 11, 22),
+            score: Window::new(12, 0, 11, 2),
+            level: Window::new(12, 4, 11, 2),
+            lines: Window::new(12, 8, 11, 2),
+            next_piece: Window::new(12, 12, 11, 6)
+        }
+    }
 
-	/// Setup the default elements of the user interface 
-	pub fn setup(&self, rb: &RustBox) {
-		let style = rustbox::RB_NORMAL;
-		let fg = Color::White;
-		let bg = Color::Black;
+    /// Setup the default elements of the user interface 
+    pub fn setup(&self, rb: &RustBox) {
+        let style = rustbox::RB_NORMAL;
+        let fg = Color::White;
+        let bg = Color::Black;
 
-		// Print out the borders for the player stats
-		self.board.print_borders(rb, style, fg, bg);
-		self.score.print_borders(rb, style, fg, bg);
-		self.level.print_borders(rb, style, fg, bg);
-		self.lines.print_borders(rb, style, fg, bg);
-		self.next_piece.print_borders(rb, style, fg, bg);
+        // Print out the borders for the player stats
+        self.board.print_borders(rb, style, fg, bg);
+        self.score.print_borders(rb, style, fg, bg);
+        self.level.print_borders(rb, style, fg, bg);
+        self.lines.print_borders(rb, style, fg, bg);
+        self.next_piece.print_borders(rb, style, fg, bg);
 
-		// Print out the initial values for the player stats
-		self.update_score(rb, 0, style, fg, bg);
-		self.update_level(rb, 0, style, fg, bg);
-		self.update_lines(rb, 0, style, fg, bg);
-	}
+        // Print out the initial values for the player stats
+        self.update_score(rb, 0, style, fg, bg);
+        self.update_level(rb, 0, style, fg, bg);
+        self.update_lines(rb, 0, style, fg, bg);
+    }
 
-	/// Update the score display to a specified value
-	pub fn update_score(&self, rb: &RustBox, score: u32, style: Style, fg: Color, bg: Color) {
-		self.score.print(rb, 1, 1, style, fg, bg, &format!("{:010}", score));
-	}
+    /// Update the score display to a specified value
+    pub fn update_score(&self, rb: &RustBox, score: u32, style: Style, fg: Color, bg: Color) {
+        self.score.print(rb, 1, 1, style, fg, bg, &format!("{:010}", score));
+    }
 
-	/// Update the level display to a specified value
-	pub fn update_level(&self, rb: &RustBox, level: u8, style: Style, fg: Color, bg: Color) {
-		self.level.print(rb, 1, 1, style, fg, bg, &format!("{:}", level));
-	}
+    /// Update the level display to a specified value
+    pub fn update_level(&self, rb: &RustBox, level: u8, style: Style, fg: Color, bg: Color) {
+        self.level.print(rb, 1, 1, style, fg, bg, &format!("{:}", level));
+    }
 
-	/// Update the current number of lines cleared display to a specified value
-	pub fn update_lines(&self, rb: &RustBox, lines: u8, style: Style, fg: Color, bg: Color) {
-		self.lines.print(rb, 1, 1, style, fg, bg, &format!("{:}", lines));
-	}
+    /// Update the current number of lines cleared display to a specified value
+    pub fn update_lines(&self, rb: &RustBox, lines: u8, style: Style, fg: Color, bg: Color) {
+        self.lines.print(rb, 1, 1, style, fg, bg, &format!("{:}", lines));
+    }
 }
--- src/window.rs
+++ src/window.rs
@@ -14,57 +14,57 @@ pub struct Window {
 
 impl Window {
 
-	/// Initializes a new Window struct at an (x, y) position with a specified width and height
-	pub fn new(x: usize, y: usize, w: usize, h: usize) -> Self {
-		Window {
-			x: x,
-			y: y,
-			w: w,
-			h: h,
-		}
-	}
+    /// Initializes a new Window struct at an (x, y) position with a specified width and height
+    pub fn new(x: usize, y: usize, w: usize, h: usize) -> Self {
+        Window {
+            x: x,
+            y: y,
+            w: w,
+            h: h,
+        }
+    }
 
     /// Prints a character at an (x, y) position relative to the origin of the Window
-	pub fn print_char(&self, rb: &RustBox, x: usize, y: usize, style: Style, fg: Color, bg: Color, c: char) {
-		// x and y are unsigned, so we do not need to check for negative values
-		if x <= self.w && y <= self.h {
-		    rb.print_char(self.x + x, self.y + y, style, fg, bg, c);
-		}
-	}
+    pub fn print_char(&self, rb: &RustBox, x: usize, y: usize, style: Style, fg: Color, bg: Color, c: char) {
+        // x and y are unsigned, so we do not need to check for negative values
+        if x <= self.w && y <= self.h {
+            rb.print_char(self.x + x, self.y + y, style, fg, bg, c);
+        }
+    }
 
     /// Prints a string at an (x, y) position relative to the origin of the Window
-	pub fn print(&self, rb: &RustBox, x: usize, y: usize, style: Style, fg: Color, bg: Color, s: &str) {
-		// x and y are unsigned, so we do not need to check for negative values
-		if x + s.len() <= self.w && y <= self.h {
-		    rb.print(self.x + x, self.y + y, style, fg, bg, s);
-		}
-	}
+    pub fn print(&self, rb: &RustBox, x: usize, y: usize, style: Style, fg: Color, bg: Color, s: &str) {
+        // x and y are unsigned, so we do not need to check for negative values
+        if x + s.len() <= self.w && y <= self.h {
+            rb.print(self.x + x, self.y + y, style, fg, bg, s);
+        }
+    }
 
     /// Convenience method to print a border around the area of the Window
-	pub fn print_borders(&self, rb: &RustBox, style: Style, fg: Color, bg: Color) {
+    pub fn print_borders(&self, rb: &RustBox, style: Style, fg: Color, bg: Color) {
 
-		// Draw the top border
-		self.print_char(rb, 0, 0, style, fg, bg, '┌');
+        // Draw the top border
+        self.print_char(rb, 0, 0, style, fg, bg, '┌');
 
-		for i in 1..self.w {
-			self.print_char(rb, i, 0, style, fg, bg, '─');
-		}
+        for i in 1..self.w {
+            self.print_char(rb, i, 0, style, fg, bg, '─');
+        }
 
-		self.print_char(rb, self.w, 0, style, fg, bg, '┐');
+        self.print_char(rb, self.w, 0, style, fg, bg, '┐');
 
         // Draw the side borders
-		for i in 1..self.h {
-			self.print_char(rb, 0, i, style, fg, bg, '│');
-			self.print_char(rb, self.w, i, style, fg, bg, '│');
-		}
+        for i in 1..self.h {
+            self.print_char(rb, 0, i, style, fg, bg, '│');
+            self.print_char(rb, self.w, i, style, fg, bg, '│');
+        }
 
-		// Draw the bottom border
-		self.print_char(rb, 0, self.h, style, fg, bg, '└');
+        // Draw the bottom border
+        self.print_char(rb, 0, self.h, style, fg, bg, '└');
 
-		for i in 1..self.w {
-			self.print_char(rb, i, self.h, style, fg, bg, '─');
-		}
+        for i in 1..self.w {
+            self.print_char(rb, i, self.h, style, fg, bg, '─');
+        }
 
-		self.print_char(rb, self.w, self.h, style, fg, bg, '┘');
-	}
+        self.print_char(rb, self.w, self.h, style, fg, bg, '┘');
+    }
 }
